da1d6bfe37360642797970e8f5c3ad556bbcc861
Vince Juric <vince.nightshift@gmail.com>
2025-06-08 00:58:20 +0200

    fix: add gap to language switcher dropdown for improved spacing

--- components/LanugageSwitcher.tsx
+++ components/LanugageSwitcher.tsx
@@ -28,7 +28,7 @@ export function LanguageSwitcher() {
         <IconLanguage />
       </div>
 
-      <ul class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+      <ul class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 gap-1.5 shadow-sm">
         {supportedLanguages.map((lang) => (
           <li>
             <a
