9ee81ca909211d010854a91f4944e05e068c5a84
Vince Juric <vince.nightshift@gmail.com>
2025-06-26 00:33:25 +0200

    refactor: update DialogTrigger component to use VNode type for children and improve onClick handler typing

--- components/Dialog/DialogTrigger.tsx
+++ components/Dialog/DialogTrigger.tsx
@@ -1,18 +1,19 @@
 import {
   cloneElement,
-  type ComponentChild,
   Fragment,
   isValidElement,
+  type JSX,
+  type VNode,
 } from "preact";
-import { useDialogContext } from "./dialogContext.ts";
 import { createDialogChangeEvent } from "./Dialog.tsx";
+import { useDialogContext } from "./dialogContext.ts";
 
 interface DialogTriggerProps {
-  children: ComponentChild;
+  children: VNode<JSX.HTMLAttributes<HTMLElement>>;
   triggerAction?: "open" | "close";
 }
 
-function getTriggerChild(children: ComponentChild) {
+function getTriggerChild(children: VNode<JSX.HTMLAttributes<HTMLElement>>) {
   if (!isValidElement(children) || children.type === Fragment) {
     throw new Error("DialogTrigger must have a single valid child element.");
   }
@@ -21,7 +22,7 @@ function getTriggerChild(children: ComponentChild) {
 }
 
 function applyTriggerPropsToChild(
-  child: ComponentChild,
+  child: VNode<JSX.HTMLAttributes<HTMLElement>>,
   triggerChildProps: Record<string, unknown>,
 ) {
   if (!isValidElement(child) || child.type === Fragment) {
@@ -38,10 +39,14 @@ export function DialogTrigger(props: DialogTriggerProps) {
 
   const { requestOpenChange } = useDialogContext();
 
-  const onClickHandler = () => {
+  const onClickHandler = (e: JSX.TargetedMouseEvent<HTMLElement>) => {
     requestOpenChange(createDialogChangeEvent(
       triggerAction === "open" ? true : false,
     ));
+
+    if (child.props.onClick) {
+      child.props.onClick(e);
+    }
   };
 
   const triggerChildProps = {
