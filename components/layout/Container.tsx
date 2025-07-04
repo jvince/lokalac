5e5d890f51e12181762e3c677987a865f89d88bb
Vince Juric <vince.nightshift@gmail.com>
2025-07-04 15:22:35 +0200

    refactor: extract className computation in Container component for improved readability

--- components/layout/Container.tsx
+++ components/layout/Container.tsx
@@ -1,12 +1,21 @@
 import type { ComponentChildren, JSX } from "preact";
+import { clsx } from "clsx/lite";
 
 interface ContainerProps extends JSX.HTMLAttributes<HTMLDivElement> {
   children?: ComponentChildren;
 }
 
 export function Container(props: ContainerProps) {
+  const className = clsx(
+    "max-w-[80rem]",
+    "h-full",
+    "mx-auto",
+    "px-6",
+    props.class || props.className,
+  );
+
   return (
-    <div class="max-w-[80rem] mx-auto px-6">
+    <div class={className}>
       {props.children}
     </div>
   );
