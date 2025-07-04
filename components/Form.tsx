416140c51883bfe7db059ba88a344a4a1e94e2fb
Vince Juric <vince.nightshift@gmail.com>
2025-07-05 00:36:20 +0200

    refactor: reorganize Form component props for improved clarity and structure

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,13 +1,27 @@
+import {
+  createInternalUrl,
+  createLanguageUrl,
+  toRelativeUrl,
+} from "$utils/url.ts";
 import type { JSX } from "preact";
-
 interface FormProps extends JSX.FormHTMLAttributes<HTMLFormElement> {}
 
 export function Form(props: FormProps) {
-  const { lang, ...restProps } = props;
+  const {
+    action,
+    lang,
+    ...restProps
+  } = props;
 
   return (
     <form
       {...restProps}
+      action={toRelativeUrl(
+        createLanguageUrl(
+          createInternalUrl(action?.toString()),
+          lang?.toString(),
+        ),
+      )}
     >
       {props.children}
       {lang && <input type="hidden" name="lang" value={lang} />}
