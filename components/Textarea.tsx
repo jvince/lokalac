6439719ceef8421cf297867975efe35042a13b57
Vince Juric <vince.nightshift@gmail.com>
2025-07-04 21:18:04 +0200

    feat: add Textarea component with theming support and Tailwind styles

---
+++ components/Textarea.tsx
@@ -0,0 +1,40 @@
+import { defineThemeProps, withTheme } from "$utils/theme.tsx";
+import type { JSX } from "preact";
+import { colorsUI, sizesUI } from "$types/daisyui.ts";
+
+/**
+ * @fileoverview
+ * Tailwind class names:
+ * - textarea
+ * - textarea-ghost
+ * - textarea-neutral
+ * - textarea-primary
+ * - textarea-secondary
+ * - textarea-accent
+ * - textarea-info
+ * - textarea-success
+ * - textarea-warning
+ * - textarea-error
+ * - textarea-xs
+ * - textarea-sm
+ * - textarea-md
+ * - textarea-lg
+ * - textarea-xl
+ */
+
+interface TextareaProps
+  extends JSX.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+function TextareaBase(props: TextareaProps) {
+  return <textarea {...props} />;
+}
+
+export const Textarea = withTheme(
+  TextareaBase,
+  "textarea",
+  defineThemeProps({
+    color: colorsUI,
+    size: sizesUI,
+    variant: ["ghost"],
+  }),
+);
