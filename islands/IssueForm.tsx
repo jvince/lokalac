b3ca2ef12e4bd0377bb85f09c1724572074299cd
Vince Juric <vince.nightshift@gmail.com>
2025-07-04 22:13:27 +0200

    refactor: streamline IssueForm component structure and improve readability

--- islands/IssueForm.tsx
+++ islands/IssueForm.tsx
@@ -95,184 +95,181 @@ export function IssueForm(props: IssueFormProps) {
   });
 
   return (
-    <>
-      {JSON.stringify(formState.$canSubmit)}
-      <Form
-        method="POST"
-        action="/submit-issue"
-        lang={props.i18nState.language.code}
-        f-client-nav={false}
-      >
-        <fieldset class="fieldset gap-y-4">
-          <legend class="fieldset-legend">
-            Create an Issue
-          </legend>
+    <Form
+      method="POST"
+      action="/issues/submit"
+      lang={props.i18nState.language.code}
+      f-client-nav={false}
+    >
+      <fieldset class="fieldset gap-y-4">
+        <legend class="fieldset-legend">
+          Create an Issue
+        </legend>
 
-          <Select
-            fullWidth
-            label={t("common.local_community")}
-            name="local_community"
-            required
-            size="lg"
-            onChange={onChangeHandler("localCommunity")}
-          >
-            <option disabled selected value="">
-              {t("common.select_local_community")}
+        <Select
+          fullWidth
+          label={t("common.local_community")}
+          name="local_community"
+          required
+          size="lg"
+          onChange={onChangeHandler("localCommunity")}
+        >
+          <option disabled selected value="">
+            {t("common.select_local_community")}
+          </option>
+          {props.communities.map((community) => (
+            <option value={community.id} key={community.id}>
+              {fromObject(community, "name")}
             </option>
-            {props.communities.map((community) => (
-              <option value={community.id} key={community.id}>
-                {fromObject(community, "name")}
+          ))}
+        </Select>
+
+        <Select
+          fullWidth
+          label={t("common.issue_category")}
+          name="issue_category"
+          required
+          size="lg"
+          onChange={onChangeHandler("issueCategory")}
+        >
+          <option disabled selected value="">
+            {t("common.select_issue_category")}
+          </option>
+          {props.categories.map((category) => (
+            <option value={category.id} key={category.id}>
+              {fromObject(category, "name")}
+            </option>
+          ))}
+        </Select>
+
+        <Select
+          fullWidth
+          label={t("common.issue_type")}
+          name="issue_type"
+          required
+          size="lg"
+          onChange={onChangeHandler("issueType")}
+        >
+          <option disabled selected value="">
+            {t("common.select_issue_type")}
+          </option>
+          {props.issueTypes
+            .filter((issueType) =>
+              issueType.category === formState.issueCategory
+            )
+            .map((issue) => (
+              <option value={issue.id} key={issue.id}>
+                {fromObject(issue, "name")}
               </option>
             ))}
-          </Select>
+        </Select>
 
-          <Select
-            fullWidth
-            label={t("common.issue_category")}
-            name="issue_category"
-            required
-            size="lg"
-            onChange={onChangeHandler("issueCategory")}
-          >
-            <option disabled selected value="">
-              {t("common.select_issue_category")}
-            </option>
-            {props.categories.map((category) => (
-              <option value={category.id} key={category.id}>
-                {fromObject(category, "name")}
-              </option>
-            ))}
-          </Select>
-
-          <Select
-            fullWidth
-            label={t("common.issue_type")}
-            name="issue_type"
-            required
-            size="lg"
-            onChange={onChangeHandler("issueType")}
-          >
-            <option disabled selected value="">
-              {t("common.select_issue_type")}
-            </option>
-            {props.issueTypes
-              .filter((issueType) =>
-                issueType.category === formState.issueCategory
-              )
-              .map((issue) => (
-                <option value={issue.id} key={issue.id}>
-                  {fromObject(issue, "name")}
-                </option>
-              ))}
-          </Select>
-
-          <Input
-            aria-label="Location"
-            contentAfter={
-              <Button
-                aria-label={t("common.clear_location")}
-                color="warning"
-                disabled={!formState.location}
-                size="lg"
-                title={t("common.clear_location")}
-                onClick={() => {
-                  formState.location = null;
-                }}
-              >
-                <span aria-hidden="true">
-                  <MapPinOffIcon />
-                </span>
-              </Button>
-            }
-            contentBefore={
-              <Button
-                aria-label={t("common.select_location")}
-                color="secondary"
-                disabled={!formState.localCommunity}
-                size="lg"
-                title={t("common.select_location")}
-                onClick={() => {
-                  isDialogOpen.value = true;
-                }}
-              >
-                <span aria-hidden="true">
-                  <MapPinPlusIcon />
-                </span>
-              </Button>
-            }
-            disabled
-            fullWidth
-            label={t("common.location")}
-            size="lg"
-            value={formState.$locationFormatted?.value ??
-              t("common.no_location_selected")}
-          />
-          <Dialog
-            open={isDialogOpen}
-            onOpenChange={(e) => isDialogOpen.value = e.detail.open}
-          >
-            <DialogBody>
-              <DialogContent>
-                <Suspense fallback="Loading map...">
-                  {IS_BROWSER && (
-                    <LeafletMapSSR style={{ height: "100%" }}>
+        <Input
+          aria-label="Location"
+          contentAfter={
+            <Button
+              aria-label={t("common.clear_location")}
+              color="warning"
+              disabled={!formState.location}
+              size="lg"
+              title={t("common.clear_location")}
+              onClick={() => {
+                formState.location = null;
+              }}
+            >
+              <span aria-hidden="true">
+                <MapPinOffIcon />
+              </span>
+            </Button>
+          }
+          contentBefore={
+            <Button
+              aria-label={t("common.select_location")}
+              color="secondary"
+              disabled={!formState.localCommunity}
+              size="lg"
+              title={t("common.select_location")}
+              onClick={() => {
+                isDialogOpen.value = true;
+              }}
+            >
+              <span aria-hidden="true">
+                <MapPinPlusIcon />
+              </span>
+            </Button>
+          }
+          disabled
+          fullWidth
+          label={t("common.location")}
+          size="lg"
+          value={formState.$locationFormatted?.value ??
+            t("common.no_location_selected")}
+        />
+        <Dialog
+          open={isDialogOpen}
+          onOpenChange={(e) => isDialogOpen.value = e.detail.open}
+        >
+          <DialogBody>
+            <DialogContent>
+              <Suspense fallback="Loading map...">
+                {IS_BROWSER && (
+                  <LeafletMapSSR style={{ height: "100%" }}>
+                    <Suspense fallback={null}>
+                      <CommunityVectorLayerSSR
+                        positions={data as LatLngTuple[]}
+                        onClick={(_, data) => {
+                          formState.location = data;
+                        }}
+                      />
                       <Suspense fallback={null}>
-                        <CommunityVectorLayerSSR
-                          positions={data as LatLngTuple[]}
-                          onClick={(_, data) => {
-                            formState.location = data;
-                          }}
-                        />
-                        <Suspense fallback={null}>
-                          {formState.location && (
-                            <MarkerSSR position={formState.location} />
-                          )}
-                        </Suspense>
+                        {formState.location && (
+                          <MarkerSSR position={formState.location} />
+                        )}
                       </Suspense>
-                    </LeafletMapSSR>
-                  )}
-                </Suspense>
-              </DialogContent>
+                    </Suspense>
+                  </LeafletMapSSR>
+                )}
+              </Suspense>
+            </DialogContent>
 
-              <DialogActions>
-                <DialogTrigger triggerAction="close">
-                  <Button autoFocus size="lg">Close</Button>
-                </DialogTrigger>
-                <Button
-                  color="primary"
-                  size="lg"
-                  onClick={() => {
-                    // Do something on Ok
-                    isDialogOpen.value = false;
-                  }}
-                >
-                  Ok
-                </Button>
-              </DialogActions>
-            </DialogBody>
-          </Dialog>
+            <DialogActions>
+              <DialogTrigger triggerAction="close">
+                <Button autoFocus size="lg">Close</Button>
+              </DialogTrigger>
+              <Button
+                color="primary"
+                size="lg"
+                onClick={() => {
+                  // Do something on Ok
+                  isDialogOpen.value = false;
+                }}
+              >
+                Ok
+              </Button>
+            </DialogActions>
+          </DialogBody>
+        </Dialog>
 
-          <Label as="label" for="note" size="lg">
-            {t("common.note")}
-          </Label>
-          <Textarea id="note" name="note" />
+        <Label as="label" for="note" size="lg">
+          {t("common.note")}
+        </Label>
+        <Textarea id="note" name="note" />
 
-          <Button
-            color="primary"
-            fullWidth
-            size="lg"
-            type="submit"
-          >
-            {t("common.submit")}
-          </Button>
+        <Button
+          color="primary"
+          fullWidth
+          size="lg"
+          type="submit"
+        >
+          {t("common.submit")}
+        </Button>
 
-          <input
-            type="hidden"
-            name="location"
-            value={JSON.stringify(formState.location)}
-          />
-        </fieldset>
-      </Form>
-    </>
+        <input
+          type="hidden"
+          name="location"
+          value={JSON.stringify(formState.location)}
+        />
+      </fieldset>
+    </Form>
   );
 }
