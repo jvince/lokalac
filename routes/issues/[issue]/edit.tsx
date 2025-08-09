60d6bfdda7bc1dec928dc2ffa6bb0a7222d414c7
Vince Juric <vince.nightshift@gmail.com>
2025-08-09 15:17:20 +0200

    feat: refactor edit page to use WithAuthorization and improve localization

--- routes/issues/[issue]/edit.tsx
+++ routes/issues/[issue]/edit.tsx
@@ -1,34 +1,38 @@
-import { Handlers, PageProps } from "$fresh/server.ts";
-import { AppState } from "$types/app.ts";
-import { getIssueById, type Issue } from "$models/issue.ts";
-import { basicAuth } from "../../../auth/basicAuth.ts";
-
-interface PageData {
-  issue: Issue | null;
-}
+import { Link } from "$components/Link.tsx";
+import { Handlers } from "$fresh/server.ts";
+import { useTranslation } from "$hooks/useTranslation.ts";
+import { getIssueById } from "$models/issue.ts";
+import { WithAuthorization } from "../../../auth/withAuthorization.ts";
+import { IssuePageProps } from "./types.ts";
 
 export const handler: Handlers = {
-  async GET(req, ctx) {
-    const unauthorized = basicAuth(req);
-
-    if (unauthorized) {
-      return unauthorized;
-    }
-
+  GET: WithAuthorization(async (_, ctx) => {
     const issue = await getIssueById(ctx.params.issue);
     return await ctx.render({ issue });
-  },
+  }),
 };
 
-export default function EditPage(props: PageProps<PageData, AppState>) {
-  if (!props.data.issue) {
-    return <div>Issue not found</div>;
+export default function EditPage(props: IssuePageProps) {
+  const { data } = props;
+  const { t, language } = useTranslation();
+
+  if (!data.issue) {
+    return <div>{t("common.issue_not_found")}</div>;
   }
 
   return (
     <div>
       {JSON.stringify(props.data.issue, null, 2)}
       <h1>Edit Issue</h1>
+
+      <Link
+        as="btn"
+        color="warning"
+        href={`/issues/${data.issue.id}/delete`}
+        lang={language.code}
+      >
+        {t("common.delete")}
+      </Link>
     </div>
   );
 }
